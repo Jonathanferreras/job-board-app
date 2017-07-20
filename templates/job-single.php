8745de9caa368eeeda34bbaeb83c785901f5f266
jonathanferreras <jonathanferreras@icloud.com>
2017-07-20 17:19:16 -0400

    added functionality of editing and deleting job listings

--- templates/job-single.php
+++ templates/job-single.php
@@ -11,21 +11,32 @@
   <hr>
   <p class="lead">
     <?php echo $job->description; ?>
-    <ul class="list-group">
-      <li class="list-group-item">
-        <strong>Company:</strong>
-        <?php echo $job->company; ?>
-      </li>
-      <li class="list-group-item">
-        <strong>Salary:</strong>
-        <?php echo $job->salary; ?>
-      </li>
-      <li class="list-group-item">
-        <strong>Email:</strong>
-        <?php echo $job->contact_email; ?>
-      </li>
-    </ul>
-    <a href="index.php" class="btn btn-default">Go back</a>
   </p>
+  <ul class="list-group">
+    <li class="list-group-item">
+      <strong>Company:</strong>
+      <?php echo $job->company; ?>
+    </li>
+    <li class="list-group-item">
+      <strong>Salary:</strong>
+      <?php echo $job->salary; ?>
+    </li>
+    <li class="list-group-item">
+      <strong>Email:</strong>
+      <?php echo $job->contact_email; ?>
+    </li>
+  </ul>
+  <a href="index.php" class="btn btn-default">Go back</a>
+  <br><br>
+  <div class="well">
+    <a href="edit.php?id=<?php echo $job->id; ?>" class="btn btn-default">
+      Edit
+    </a>
+    <form action="job.php" method="post" style="display:inline;">
+      <input type="hidden" name="del_id" value="<?php echo $job->id; ?>">
+      <input type="submit" class="btn btn-danger" value="Delete">
+    </form>
+  </div>
+
 
 <?php include 'inc/footer.php'; ?>
